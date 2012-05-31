ce1f1422a4c31a5a8eb3dec6722c7067257097f0
Tyler Sommer <sommertm@gmail.com>
2012-05-31 13:29:37 -0600

    Moved contact related fields on CardAccount to AbstractAccount

--- lib/Orkestra/Transactor/Entity/AbstractAccount.php
+++ lib/Orkestra/Transactor/Entity/AbstractAccount.php
@@ -36,6 +36,55 @@ abstract class AbstractAccount extends EntityBase
     protected $ipAddress = '';
 
     /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string")
+     */
+    protected $name = '';
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="address", type="string")
+     */
+    protected $address = '';
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="city", type="string")
+     */
+    protected $city = '';
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="region", type="string", length=3)
+     */
+    protected $region = '';
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="country", type="string", length=3)
+     */
+    protected $country = '';
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="postal_code", type="string")
+     */
+    protected $postalCode = '';
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="phoneNumber", type="string")
+     */
+    protected $phoneNumber = '';
+
+    /**
      * @var \Orkestra\Transactor\Entity\Transaction
      *
      * @ORM\OneToMany(targetEntity="Orkestra\Transactor\Entity\Transaction", mappedBy="account", cascade={"persist"})
@@ -123,5 +172,145 @@ abstract class AbstractAccount extends EntityBase
     public function getTransactions()
     {
         return $this->transactions;
+    }
+
+    /**
+     * Set Phone Number
+     *
+     * @param string $phoneNumber
+     */
+    public function setPhoneNumber($phoneNumber)
+    {
+        $this->phoneNumber = $phoneNumber;
+    }
+
+    /**
+     * Get Phone Number
+     *
+     * @return string
+     */
+    public function getPhoneNumber()
+    {
+        return $this->phoneNumber;
+    }
+
+    /**
+     * Sets the name
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Gets the name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Sets the address
+     *
+     * @param string $address
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    }
+
+    /**
+     * Gets the address
+     *
+     * @return string
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * Sets the city
+     *
+     * @param string $city
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+    }
+
+    /**
+     * Gets the city
+     *
+     * @return string
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Sets the postal code
+     *
+     * @param string $postalCode
+     */
+    public function setPostalCode($postalCode)
+    {
+        $this->postalCode = $postalCode;
+    }
+
+    /**
+     * Gets the postal code
+     *
+     * @return string
+     */
+    public function getPostalCode()
+    {
+        return $this->postalCode;
+    }
+
+    /**
+     * Sets the region
+     *
+     * @param string $region
+     */
+    public function setRegion($region)
+    {
+        $this->region = $region;
+    }
+
+    /**
+     * Gets the region
+     *
+     * @return string
+     */
+    public function getRegion()
+    {
+        return $this->region;
+    }
+
+    /**
+     * Sets the country
+     *
+     * @param string $country
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+    }
+
+    /**
+     * Gets the country
+     *
+     * @return string
+     */
+    public function getCountry()
+    {
+        return $this->country;
     }
 }
