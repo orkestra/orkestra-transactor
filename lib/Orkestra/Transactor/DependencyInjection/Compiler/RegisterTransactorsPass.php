0fddc2a6791126faaeabfee438a62d28c37ddb78
Tyler Sommer <sommertm@gmail.com>
2012-07-02 11:51:24 -0600

    Moved RegisterTransactors compiler pass from TransactorBundle

---
+++ lib/Orkestra/Transactor/DependencyInjection/Compiler/RegisterTransactorsPass.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Orkestra\Transactor\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+
+/**
+ * Registers all Transactors with the TransactorFactory
+ */
+class RegisterTransactorsPass implements CompilerPassInterface
+{
+    /**
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     *
+     * @return mixed
+     */
+    public function process(ContainerBuilder $container)
+    {
+        if (!$container->hasDefinition('orkestra.transactor_factory')) {
+            return;
+        }
+
+        $definition = $container->getDefinition('orkestra.transactor_factory');
+
+        foreach ($container->findTaggedServiceIds('orkestra.transactor') as $service => $tags) {
+            $definition->addMethodCall('registerTransactor', array(new Reference($service)));
+        }
+    }
+}
