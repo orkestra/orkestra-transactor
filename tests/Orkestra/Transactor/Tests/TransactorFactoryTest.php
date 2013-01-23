f1d213d7b9e77fc266ea1b3cfc769492d2b32b1f
Tyler Sommer <sommertm@gmail.com>
2013-01-23 10:57:45 -0800

    Added LICENSE

--- tests/Orkestra/Transactor/Tests/TransactorFactoryTest.php
+++ tests/Orkestra/Transactor/Tests/TransactorFactoryTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Orkestra Transactor package.
+ *
+ * Copyright (c) Orkestra Community
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
 namespace Orkestra\Transactor\Tests;
 
 use Orkestra\Transactor\TransactorFactory;
