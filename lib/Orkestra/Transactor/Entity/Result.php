orkestra/orkestra-transactor

commit a96e469a726374aecb3feaac4c288b1c062eed97

Fixed issue where transactor might not be set when a transaction is processed

lib/Orkestra/Transactor/Entity/Result.php
@@ -5,7 +5,7 @@ namespace Orkestra\Transactor\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Orkestra\Common\Type\DateTime;
 use Orkestra\Common\Type\NullDateTime;
-use Orkestra\Common\Entity\AbstractEntity;
+use Orkestra\Common\Entity\EntityBase;
 use Orkestra\Transactor\TransactorInterface;
 
 /**
@@ -14,7 +14,7 @@ use Orkestra\Transactor\TransactorInterface;
  * @ORM\Table(name="orkestra_results")
  * @ORM\Entity
  */
-class Result extends AbstractEntity
+class Result extends EntityBase
 {
     /**
      * @var string $externalId
@@ -124,7 +124,7 @@ class Result extends AbstractEntity
      */
     public function getTransaction()
     {
-    	return $this->transaction;
+        return $this->transaction;
     }
 
     /**
@@ -239,5 +239,13 @@ class Result extends AbstractEntity
     public function setTransactor($transactor)
     {
         $this->transactor = is_object($transactor) ? $transactor->getType() : $transactor;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDateTransacted()
+    {
+        return $this->dateTransacted;
     }
 }

lib/Orkestra/Transactor/AbstractTransactor.php
@@ -42,6 +42,7 @@ abstract class AbstractTransactor implements TransactorInterface
         }
 
         $result = $transaction->getResult();
+        $result->setTransactor($this);
 
         try {
             $this->_doTransact($transaction, $options);

tests/Orkestra/Transactor/Tests/AbstractTransactorTest.php
@@ -82,6 +82,7 @@ class AbstractTransactorTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('An internal error occurred while processing the transaction.', $result->getMessage());
         $this->assertEquals('Critical error', $result->getData('message'));
         $this->assertNotEmpty($result->getData('trace'));
+        $this->assertNotEmpty($result->getTransactor());
     }
 }
 

commit 27ace25e7ae0539672ea22de1af83b0fff2cdbbb

Fixed incorrect classname introduced in a96e469

lib/Orkestra/Transactor/Entity/Result.php
@@ -5,7 +5,7 @@ namespace Orkestra\Transactor\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Orkestra\Common\Type\DateTime;
 use Orkestra\Common\Type\NullDateTime;
-use Orkestra\Common\Entity\EntityBase;
+use Orkestra\Common\Entity\AbstractEntity;
 use Orkestra\Transactor\TransactorInterface;
 
 /**
@@ -14,7 +14,7 @@ use Orkestra\Transactor\TransactorInterface;
  * @ORM\Table(name="orkestra_results")
  * @ORM\Entity
  */
-class Result extends EntityBase
+class Result extends AbstractEntity
 {
     /**
      * @var string $externalId