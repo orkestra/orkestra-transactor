orkestra/orkestra-transactor

commit f1f29a3bd060b1733573fcb3ea3781b7f156a868

Changed option keys in transactors to underscorized

lib/Orkestra/Transactor/Transactor/NetworkMerchants/CardTransactor.php
@@ -251,7 +251,7 @@ class CardTransactor extends AbstractTransactor
         $resolver->setDefaults(array(
             'enable_avs' => false,
             'enable_cvv' => false,
-            'postUrl'    => 'https://secure.networkmerchants.com/api/transact.php',
+            'post_url'   => 'https://secure.networkmerchants.com/api/transact.php',
         ));
     }
 

lib/Orkestra/Transactor/Transactor/PaymentsXpress/AchTransactor.php
@@ -386,9 +386,9 @@ class AchTransactor extends AbstractTransactor
     protected function configureResolver(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
-            'testMode'   => false,
+            'test_mode'  => false,
             'date'       => null,
-            'postUrl'    => 'https://www.paymentsxpress.com/pxgateway/datalinks/transact.aspx',
+            'post_url'   => 'https://www.paymentsxpress.com/pxgateway/datalinks/transact.aspx',
         ));
     }
 

commit d7f1bf7e331fe8b5ee233c8d71cfb61c19205814

Fixed issue introduced in f1f29a3

lib/Orkestra/Transactor/Transactor/NetworkMerchants/CardTransactor.php
@@ -76,7 +76,7 @@ class CardTransactor extends AbstractTransactor
         $result = $transaction->getResult();
         $result->setTransactor($this);
 
-        $postUrl = $options['postUrl'];
+        $postUrl = $options['post_url'];
         $client = $this->getClient();
 
         $request = $client->post($postUrl)

lib/Orkestra/Transactor/Transactor/PaymentsXpress/AchTransactor.php
@@ -78,7 +78,7 @@ class AchTransactor extends AbstractTransactor
         $result = $transaction->getResult();
         $result->setTransactor($this);
 
-        $postUrl = $options['postUrl'];
+        $postUrl = $options['post_url'];
         $client = $this->getClient();
 
         $request = $client->post($postUrl)
@@ -193,7 +193,7 @@ class AchTransactor extends AbstractTransactor
             'Provider_GateKey' => $credentials->getCredential('providerGateKey'),
             'Command' => $this->_getCommand($transaction),
             'CommandVersion' => '1.0',
-            'TestMode' => !empty($options['testMode']) ? 'On' : 'Off',
+            'TestMode' => !empty($options['test_mode']) ? 'On' : 'Off',
             'MerchantID' => $credentials->getCredential('merchantId'),
             'Merchant_GateID' => $credentials->getCredential('merchantGateId'),
             'Merchant_GateKey' => $credentials->getCredential('merchantGateKey'),