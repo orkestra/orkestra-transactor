40ffe267e854079526a78d68541dc38708e585d9
Tyler Sommer <sommertm@gmail.com>
2013-03-29 08:41:58 -0700

    Added an example implementation using NMI

---
+++ examples/basic.php
@@ -0,0 +1,62 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Orkestra\Transactor\Entity\Account\CardAccount;
+use Orkestra\Transactor\Entity\Result\ResultStatus;
+use Orkestra\Transactor\Entity\Transaction;
+use Orkestra\Transactor\Transactor\NetworkMerchants;
+use Orkestra\Transactor\Type\Month;
+use Orkestra\Transactor\Type\Year;
+
+$transactor = new NetworkMerchants\CardTransactor();
+
+$credentials = $transactor->createCredentials();
+/*
+ * Credentials created through a transactor come initialized with default
+ * values specific to that transactor. You can enumerate these default values:
+ *
+ * print_r($credentials->getCredentials());
+ */
+
+// Credentials support magic get/set
+$credentials->username = 'demo';
+$credentials->password = 'password';
+
+$account = new CardAccount();
+$account->setAccountNumber('4111111111111111');
+$account->setExpMonth(new Month(10));
+$account->setExpYear(new Year(2015));
+
+// Create a Credit card Sale transaction
+$transaction = new Transaction();
+$transaction->setCredentials($credentials);
+$transaction->setAccount($account);
+$transaction->setAmount(2.50);
+$transaction->setNetwork(new Transaction\NetworkType(Transaction\NetworkType::CARD));
+$transaction->setType(new Transaction\TransactionType(Transaction\TransactionType::SALE));
+
+$result = $transactor->transact($transaction);
+
+if (!assert(ResultStatus::APPROVED == $result->getStatus())) {
+    echo "Sales was " . $result->getStatus() . PHP_EOL;
+    echo "Reason: " . $result->getMessage() . PHP_EOL;
+
+    exit(1);
+}
+
+echo "Sale Approved, Transaction ID: " . $result->getExternalId() . PHP_EOL;
+
+
+// Refund the sale
+$refund = $transaction->createChild(new Transaction\TransactionType(Transaction\TransactionType::REFUND));
+
+$result = $transactor->transact($refund);
+
+if (!assert(ResultStatus::APPROVED == $result->getStatus())) {
+    echo "Refund Declined. Reason: " . $result->getMessage() . PHP_EOL;
+
+    exit(1);
+}
+
+echo "Refund Approved." . PHP_EOL;
